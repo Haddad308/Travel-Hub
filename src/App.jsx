3358fc43cdf57fdc0d4c363a351226c2c85db0e4
Mohamed Haddad <melhaddad308@gmail.com>
2024-01-04 22:32:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-cyan-500' >بسم الله الرحمن الرحيم</h1>
+      <h3>AdminPanel Project</h3>
+    </>
+  )
+}
+
+export default App
